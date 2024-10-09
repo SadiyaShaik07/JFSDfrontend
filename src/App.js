b07541eecd678f06d38762921861571640f5d166
sadiyashaik60@gmail.com <2200030479@kluniversity.in>
2024-10-09 08:25:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './main/Home';
+import About from './main/About';
+import Login from './main/Login';
+import Registration from './main/Registration';
+import ContactUs from './main/ContactUs';
+import StudentHome from './student/studenthome'
+import LogAchievements from './student/LogAchievements';
+import ViewAchievements from './student/ViewAchievements';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/registration" element={<Registration/>}/>
+           <Route path="/contactus" element={<ContactUs/>}/>
+           <Route path= "/studenthome" element={<StudentHome/>}/>
+           <Route path="/LogAchievements" element={<LogAchievements/>}/>
+           <Route path="/ViewAchievements" element={<ViewAchievements/>}/>
+         
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
